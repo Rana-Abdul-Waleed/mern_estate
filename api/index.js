fdb1028ccb1e423b26eab2398d5ec7c4bec11422
Abdul Waleed <abdulwaleedfsd@gmail.com>
2024-09-29 14:54:25 -0700

    create update user api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import userRoute from "./routes/userRoute.js";
 import authRoute from "./routes/authRoute.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -20,6 +21,8 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
